238b05cdfba70f0cce93f6d993b66c829cbc0d78
TaahaRauf <113552962+TaahaRauf@users.noreply.github.com>
2022-09-17 18:00:56 +0500

    updates

--- Assignment1/Source20.cpp
+++ Assignment1/Source20.cpp
@@ -1,30 +1,36 @@
-#include <iostream>
-using namespace std;
-int main() {
-	float speedA;
-	float speedB;
-	float minutesA;
-	float minutesB;
-	float timeA;
-	float timeB;
-	float distanceA;
-	float distanceB;
-	cout << "Enter the speed of Car A in miles per hour ";
-	cin >> speedA;
-	cout << "Enter the speed of Car B in miles per hour ";
-	cin >> speedB;
-	cout << "Enter the hours travelled by Car A ";
-	cin >> timeA;
-	cout << "Enter the hours travelled by Car B ";
-	cin >> timeB;
-	cout << "Enter the minutes travelled by Car A ";
-	cin >> minutesA;
-	timeA = timeA + (minutesA / 60);
-	cout << "Enter the minutes travelled by Car B ";
-	cin >> minutesB;
-	timeB = timeB + (minutesB / 60);
-	distanceA =
-
-
-	return 0;
-}
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//int main() {
+//	float speedA;
+//	float speedB;
+//	float minutesA;
+//	float minutesB;
+//	float timeA;
+//	float timeB;
+//	float distanceA;
+//	float distanceB;
+//	float shortestDistance;
+//	cout << "Enter the speed of Car A in miles per hour ";
+//	cin >> speedA;
+//	cout << "Enter the speed of Car B in miles per hour ";
+//	cin >> speedB;
+//	cout << "Enter the hours travelled by Car A ";
+//	cin >> timeA;
+//	cout << "Enter the hours travelled by Car B ";
+//	cin >> timeB;
+//	cout << "Enter the minutes travelled by Car A ";
+//	cin >> minutesA;
+//	timeA = timeA + (minutesA / 60);
+//	cout << "Enter the minutes travelled by Car B ";
+//	cin >> minutesB;
+//	timeB = timeB + (minutesB / 60);
+//	distanceA = speedA * timeA;
+//	distanceB = speedB * timeB;
+//	shortestDistance = sqrt(pow(distanceA, 2) + pow(distanceB, 2));
+//	cout << "The shortest distance between the two cars is " << shortestDistance;
+//
+//
+//
+//	return 0;
+//}
