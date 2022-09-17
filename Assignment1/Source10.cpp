7f02dba4f5da7c613cb2c9c87d2c0f5aad403a25
TaahaRauf <113552962+TaahaRauf@users.noreply.github.com>
2022-09-17 18:03:46 +0500

    update

--- Assignment1/Source10.cpp
+++ Assignment1/Source10.cpp
@@ -1,10 +1,11 @@
-//#include <iostream>
-//using namespace std;
-//int main() {
-//	int var1 = 5;
-//	int var2 = 10;
-//	var1 = var1 + var2;
-//	var2 = var1 - var2;
-//	var1 = var1 - var2;
-//	return 0;
-//}
+#include <iostream>
+using namespace std;
+int main() {
+	int var1 = 5;
+	int var2 = 10;
+	cout << "Var 1 before swap is " << var1 << " and Var2 is " << var2 <<endl;
+
+	swap(var1, var2);
+	cout << "Var 1 after swap is " << var1 << " and Var2 is " << var2;
+	return 0;
+}
--- Assignment1/Source21.cpp
+++ Assignment1/Source21.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <iomanip>
-using namespace std;
-
-int main()
-{
-    float rand1, rand2, answer;
-    int max;
-    srand(time(0));
-    max = 99999;
-    rand1 = rand() % max;
-    rand2 = rand() % max;
-    answer = rand1 + rand2;
-    cout << setw(10) << rand1  << endl;
-    cout << "+" << endl;
-    cout << setw(10) << rand2 << endl;
-    cout << "---------------------" << endl;
-    system("pause");
-    cout << setw(10) << answer;
-
-    return 0;
-}
+//#include <iostream>
+//#include <cstdlib>
+//#include <ctime>
+//#include <iomanip>
+//using namespace std;
+//
+//int main()
+//{
+//    float rand1, rand2, answer;
+//    int max;
+//    srand(time(0));
+//    max = 99999;
+//    rand1 = rand() % max;
+//    rand2 = rand() % max;
+//    answer = rand1 + rand2;
+//    cout << setw(10) << rand1  << endl;
+//    cout << "+" << endl;
+//    cout << setw(10) << rand2 << endl;
+//    cout << "---------------------" << endl;
+//    system("pause");
+//    cout << setw(10) << answer;
+//
+//    return 0;
+//}
